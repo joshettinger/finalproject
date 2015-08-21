986f7ba31e31f64e25cc275a9e5916ee1286b12b
joshettinger <joshettinger12@gmail.com>
2015-08-21 13:21:02 -0400

    Added JS
    To contribute page

---
+++ JS/index.js
@@ -0,0 +1,14 @@
+$("document").ready(function() {
+
+console.log("test");
+
+$(".add").click(function(){
+        $(".addAShow").toggle();
+    });
+
+
+$(".review1").click(function(){
+        $(".findapod2").toggle();
+    });
+
+});
