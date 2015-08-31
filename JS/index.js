da9c4be6a741c58ed5d216e2c31f27e36962dfa7
joshettinger <joshettinger12@gmail.com>
2015-08-31 18:23:58 -0400

    Updates 8.31

--- JS/index.js
+++ JS/index.js
@@ -6,9 +6,26 @@ $(".add").click(function(){
         $(".addAShow").toggle();
     });
 
-
 $(".review1").click(function(){
         $(".findapod2").toggle();
     });
+
+$(".podcastslist").on("mouseover", function(){
+        $(".menudropdown").css("visibility","visible");
+      });
+
+$(".menudropdown").on("mouseover", function(){
+        $(".menudropdown").css("visibility","visible");
+      });
+
+
+$(".podcastslist").on("mouseleave", function(){
+              $(".menudropdown").css("visibility","hidden");
+            });
+
+$(".menudropdown").on("mouseleave", function(){
+              $(".menudropdown").css("visibility","hidden");
+            });
+
 
 });
